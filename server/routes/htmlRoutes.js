06e342145ab49d1888241c55ba2171007052c90d
Nllano777 <nllbr99@gmail.com>
2022-11-14 19:50:26 -0600

    update in routes

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -4,3 +4,8 @@ module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
   );
+
+module.exports = (app) =>
+  app.get('/*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+  );
